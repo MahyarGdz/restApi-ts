0a9aba977c96061bff0dbf452a55c69a844cdd91
MahyarGdz <128599591+MahyarGdz@users.noreply.github.com>
2024-03-23 03:42:08 +0330

    fix bug in logging system

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,12 +1,12 @@
 import winston from "winston";
-const { combine, timestamp, json, colorize, align } = winston.format;
+const { combine, timestamp, json, align } = winston.format;
 
 export const logger = winston.createLogger({
   level: process.env.LOGGER_LEVEL,
   format: combine(
     timestamp({ format: "YYYY-MM-DD hh:mm:ss.SSS A" }),
     json({ space: 4 }),
-    align(),
+    align()
   ),
   transports: [
     new winston.transports.File({
@@ -14,15 +14,13 @@ export const logger = winston.createLogger({
     }),
     new winston.transports.File({
       filename: "logs/error.log",
-      level: "error",
-    }),
+      level: "error", 
+    }), 
   ],
 });
 
-if (process.env.NODE_ENV != "production") {
-  logger.add(
-    new winston.transports.Console({
-      format:winston.format.simple() || colorize({all:true}),
-    })
-  );
-}
+logger.add(
+  new winston.transports.Console({
+    format: winston.format.simple(),
+  })
+);
